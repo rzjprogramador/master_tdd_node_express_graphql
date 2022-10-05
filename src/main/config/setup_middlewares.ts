5c28b50b2f98b7c1a3a243db5ad007c46e51c026
rzjprogramador <rzjprogramador@gmail.com>
2022-10-05 15:46:20 -0300

    feat: [middlewares] request return default content type

--- src/main/config/setup_middlewares.ts
+++ src/main/config/setup_middlewares.ts
@@ -1,7 +1,8 @@
 import { Express } from 'express'
-import { bodyParser, cors } from '@src/main/middlewares'
+import { bodyParser, cors, contentType } from '@src/main/middlewares'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
--- src/main/middlewares/index.ts
+++ src/main/middlewares/index.ts
@@ -1,2 +1,3 @@
 export * from '@src/main/middlewares/body_parser'
 export * from '@src/main/middlewares/cors'
+export * from '@src/main/middlewares/content_type'
