df7f42bc6803f81f3ae30668e156e108628d0e5d
rzjprogramador <rzjprogramador@gmail.com>
2022-10-07 13:50:17 -0300

    feat: [exemplo1] tester exemplo1 create

--- src/entitys/exemplo1/endpoint_graphql/exemplo1.typedefs.ts
+++ src/entitys/exemplo1/endpoint_graphql/exemplo1.typedefs.ts
@@ -2,8 +2,22 @@ import { gql } from 'apollo-server-express'
 
 
 const exemplo1TypeDefs = gql`
-type Query {
+  type Exemplo1 {
+    prop1: String
+    prop2: String
+  }
+
+  input CreateExemplo1Input {
+    prop1: String
+    prop2: String
+  }
+
+  type Query {
   exemplo1Inicio: String
+  }
+
+type Mutation {
+  createExemplo1(createExemplo1Input: CreateExemplo1Input): Exemplo1
 }
 `
 
